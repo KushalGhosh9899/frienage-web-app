fa48072caab6696e784d8b91816a318445e474cd
Kushal <kushalghosh9899@gmail.com>
2021-05-08 18:00:51 +0530

    Updated Navbar Styling and Home Page Post Card Stylings

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,123 +4,160 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin');
 const Post = mongoose.model("Post");
 
-router.get('/allpost',requireLogin,(req,res)=>{
+router.get('/allpost', requireLogin, (req, res) => {
     Post.find()
-    .populate("postedBy","_id name")
-    .then(posts=>{
-        res.json({posts});
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+        .populate("postedBy", "_id name pic")
+        .populate("comments.postedBy", "_id name pic")
+        .then(posts => {
+            res.json({ posts });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
 
-router.post('/createpost',requireLogin,(req,res)=>{
-    const {title,body,photo} = req.body;
-    if(!title||!body||!photo){
-        return res.status(422).json({error:"please add all the fields"});
+router.post('/createpost', requireLogin, (req, res) => {
+    const { title, body, photo } = req.body;
+    if (!title || !body || !photo) {
+        return res.status(422).json({ error: "please add all the fields" });
     }
-    req.user.password=undefined;
+    req.user.password = undefined;
     const post = new Post({
         title,
         body,
-        photo:photo,
-        postedBy:req.user
+        photo: photo,
+        postedBy: req.user
     })
-    post.save().then(result=>{
-        res.json({post:result})
+    post.save().then(result => {
+        res.json({ post: result })
     })
-    .catch(err=>{
-        console.log(err);
-    })
+        .catch(err => {
+            console.log(err);
+        })
 })
 
-router.get('/mypost',requireLogin,(req,res)=>{
-    Post.find({postedBy:req.user._id})
-    .populate("postedBy","_id name")
-    .then(mypost=>{
-        res.json({mypost});
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+router.get('/mypost', requireLogin, (req, res) => {
+    Post.find({ postedBy: req.user._id })
+        .populate("postedBy", "_id name")
+        .then(mypost => {
+            res.json({ mypost });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
 
-router.put('/like',requireLogin,(req,res)=>{
-    Post.findByIdAndUpdate(req.body.postId,{
-        $push:{likes:req.user._id}
-    },{
-        new:true
-    }).exec((err,result)=>{
-        if(err){
-            return res.status(422).json({error:err})
-        }else{
-            res.json(result)
-        }
+router.put('/like', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push: { likes: req.user._id }
+    }, {
+        new: true
     })
+        .populate("comments.postedBy", "_id name pic")
+        .populate("postedBy", "_id name pic")
+        .exec((err, result) => {
+            if (err) {
+                return res.status(422).json({ error: err })
+            } else {
+                res.json(result)
+            }
+        })
 })
 
-router.put('/unlike',requireLogin,(req,res)=>{
-    Post.findByIdAndUpdate(req.body.postId,{
-        $pull:{likes:req.user._id}
-    },{
-        new:true
-    }).exec((err,result)=>{
-        if(err){
-            return res.status(422).json({error:err})
-        }else{
-            res.json(result)
-        }
+router.put('/unlike', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $pull: { likes: req.user._id }
+    }, {
+        new: true
     })
+        .populate("comments.postedBy", "_id name pic")
+        .populate("postedBy", "_id name pic")
+        .exec((err, result) => {
+            if (err) {
+                return res.status(422).json({ error: err })
+            } else {
+                res.json(result)
+            }
+        })
 })
 
-router.put('/comment',requireLogin,(req,res)=>{
+router.put('/comment', requireLogin, (req, res) => {
     const comment = {
-        text:req.body.text,
-        postedBy:req.user._id
+        text: req.body.text,
+        postedBy: req.user._id
     }
-    Post.findByIdAndUpdate(req.body.postId,{
-        $push:{comments:comment}
-    },{
-        new:true
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push: { comments: comment }
+    }, {
+        new: true
     })
-    .populate("comments.postedBy","_id name")
-    .populate("postedBy","_id name")
-    .exec((err,result)=>{
-        if(err){
-            return res.status(422).json({error:err})
-        }else{
-            res.json(result)
-        }
-    })
+        .populate("comments.postedBy", "_id name pic")
+        .populate("postedBy", "_id name pic")
+        .exec((err, result) => {
+            if (err) {
+                return res.status(422).json({ error: err })
+            } else {
+                res.json(result)
+            }
+        })
 })
 
-router.delete('/deletepost/:postId',requireLogin,(req,res)=>{
-    Post.findOne({_id:req.params.postId})
-    .populate("postedBy","_id")
-    .exec((err,post)=>{
-        if(err){
-            return res.status(422).json({error:err})
-        }if(post.postedBy._id.toString()==req.user._id.toString()){
-            post.remove()
-            .then(result=>{
-                res.json({result})
-            }).catch(err=>{
-                console.log(err)
-            })
-        }
-    })
+router.delete('/deletepost/:postId', requireLogin, (req, res) => {
+    Post.findOne({ _id: req.params.postId })
+        .populate("postedBy", "_id name pic")
+        .populate("comments.postedBy", "_id name pic")
+        .exec((err, post) => {
+            if (err) {
+                return res.status(422).json({ error: err })
+            } if (post.postedBy._id.toString() == req.user._id.toString()) {
+                post.remove()
+                    .then(result => {
+                        res.json({ result })
+                    }).catch(err => {
+                        console.log(err)
+                    })
+            }
+        })
 })
 
-router.get('/getsubpost',requireLogin,(req,res)=>{
-    Post.find({postedBy:{$in:req.user.following}})
-    .populate("postedBy","_id name")
-    .populate("comments.postedBy","_id name")
-    .then(posts=>{
-        res.json({posts});
+router.get('/getsubpost', requireLogin, (req, res) => {
+    Post.find({ postedBy: { $in: req.user.following } })
+        .populate("postedBy", "_id name")
+        .populate("comments.postedBy", "_id name")
+        .then(posts => {
+            res.json({ posts });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+router.put('/deletecomment', requireLogin, (req, res) => {
+    const comment = {
+        _id: req.body.commentid,
+        text: req.body.comment,
+        postedBy: req.body.postby
+    }   
+    .populate("comments.postedBy", "_id name pic")
+    .populate("postedBy", "_id name pic")
+    .exec((err, result) => {
+        if (err) {
+            return res.status(422).json({ error: err })
+        } else {
+            res.json({"success":result})
+        }
     })
-    .catch(err=>{
-        console.log(err);
-    })
+    // const result = await Post.findOne({
+    //     comments: {$elemMatch: { _id: req.params.commentId} }
+    // })
+    // const result2 = await Post.deleteOne({
+    //     comments: {$elemMatch: { _id: req.params.commentId} }
+    // })
+    // console.log("Result = ", result1)
+    // Post.findOne({"comments":{_id:req.params.commentId}})
+    // .then(result=>{
+    //     console.log(result._id)
+    // })
+    // Post.deleteOne({"comments":{_id:req.params.commentId}})
 })
 module.exports = router;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,11 @@
 import React, { useContext, useRef, useEffect, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { userContext } from '../App';
 import M from 'materialize-css';
 
 const Navbar = () => {
   const [colorChange, setColorchange] = useState(false);
+  const [dropDown, setDropDown] = useState(false);
   const changeNavbarColor = () => {
     if (window.scrollY >= 80) {
       setColorchange(true);
@@ -16,31 +17,61 @@ const Navbar = () => {
   window.addEventListener('scroll', changeNavbarColor);
 
   const searchModal = useRef(null)
+  const logoutModal = useRef(null)
   const [search, setSearch] = useState('');
   const [userDetails, setUserDetails] = useState([]);
   const history = useHistory();
   const { state, dispatch } = useContext(userContext);
   useEffect(() => {
     M.Modal.init(searchModal.current)
+    M.Modal.init(logoutModal.current)
   }, [])
+
+  const location = useLocation();
+  console.log(location.pathname)
   const renderList = () => {
     if (state) {
       return [
-        <li key="1"><Link to=""><i data-target="modal1" className="material-icons modal-trigger">search</i></Link></li>,
-        <li key="2"><Link to="/profile">Profile</Link></li>,
-        <li key="3"><Link to="/createpost">Create Post</Link></li>,
-        <li key="4"><Link to="/myfollowingpost">My Following Posts</Link></li>,
-        <button className="waves-effect waves-light btn-small red darken-1"
-          onClick={() => {
-            localStorage.clear()
-            dispatch({ type: "CLEAR" })
-            history.push('/login')
+        <div className="nav-btn">
+          <Link to="/" title="Home" className={location.pathname == '/' ? "active-btn" : ""}> <i class="material-icons">home</i></Link>
+          <Link to="/profile" title="My Profile" className={location.pathname == '/profile' ? "active-btn" : ""}><i class="material-icons">person</i></Link>
+          <Link title="Search" data-target="modal1" className="material-icons modal-trigger" >
+            <i data-target="modal1" className="material-icons modal-trigger">search</i></Link>
+          <Link to="/myfollowingpost" title="Following" className={location.pathname == '/myfollowingpost' ? "active-btn" : ""}>
+            <i class="material-icons">supervisor_account</i></Link>
+          <Link to="/messages" title="Messages" className={location.pathname == '/messages' ? "active-btn" : ""}><i class="material-icons">message</i></Link>
+          <Link to="/settings" title="Settings" className={location.pathname == '/settings' ? "active-btn" : ""}><i class="material-icons">settings</i></Link>
+        </div>,
+        <div className="profile-details">
+          <Link to="/notifications">
+            <i class="material-icons notification">notifications_none
+          <span class="new-badge">4</span></i>
+          </Link>
+          <Link to='/profile' className="profile-details">
+            <img className="author-pic-nav"
+              src={state ? state.pic : "loading.."}
+            />
+            <h2>{state.name}</h2>
+          </Link>
+          <i onClick={() => {
+            dropDown ?
+              setDropDown(false) :
+              setDropDown(true)
           }}
-        >Logout</button>
+            class="material-icons">arrow_drop_down_circle</i>
+        </div>,
+        <div className={dropDown ? "dropdown-menu z-depth-5" : "dropdown-menu z-depth-5 hide"}>
+          <span><i class="material-icons">person_outline</i>My Profile</span>
+          <div className="divider"></div>
+          <span><i class="material-icons">settings</i>Settings</span>
+          <div className="divider"></div>
+          <span data-target="logoutModal" className="modal-trigger">
+            <i class="material-icons">power_settings_new</i>Logout</span>
+        </div>
       ]
     } else {
       return [
-        <Link to="/login"><button className="btn-default light waves-effect waves-light">sign in</button></Link>
+        <Link to="/login"><button className="btn-default light waves-effect waves-light right-top">sign in</button></Link>
       ]
     }
   }
@@ -64,14 +95,14 @@ const Navbar = () => {
 
 
   return (
-    <nav className={state ? "nav-wrapper white" : "nav-wrapper white" || colorChange ? 'custom-header fixed' : 'z-depth-0 custom-header'}>
+    <nav className={state ? "nav-wrapper white" : "" || colorChange ? 'custom-header fixed' : 'z-depth-0 custom-header'}>
       <div>
 
         <Link to={state ? "/" : "/startup"} className="brand-logo logo-txt">
           Friengage
         </Link>
 
-        <ul id="nav-mobile" className="right">
+        <ul id="nav-mobile">
           {renderList()}
         </ul>
 
@@ -99,6 +130,27 @@ const Navbar = () => {
             <button className="modal-close waves-effect waves-green btn-flat" onClick={() => setSearch('')}>Close</button>
           </div>
         </div>
+
+        {/* <!-- Logout Modal Structure --> */}
+        <div id="logoutModal" className="modal logout-modals" ref={logoutModal}>
+          <div className="modal-content logout-modal">
+            <h2>
+              Are you sure you want to Logout?
+            </h2>
+            <div className="logout-buttons">
+              <button className="modal-close btn-default light waves-effect waves-light"> No </button>
+              <button className="btn-default dark waves-effect waves-light" onClick={() => {
+                localStorage.clear()
+                dispatch({ type: "CLEAR" })
+                history.push('/login')
+                M.Modal.getInstance(logoutModal.current).close()
+
+              }}
+              > Yes </button>
+            </div>
+          </div>
+        </div>
+
       </div>
     </nav>
   )
